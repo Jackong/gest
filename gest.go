1bd73f1a9657a192ba9cfe052519978717aa7f3a
Jackong <JackongC@gmail.com>
2014-11-03 18:08:21 +0800

    add gest package

---
+++ gest.go
@@ -0,0 +1,107 @@
+package gest
+
+import (
+	"net/http"
+	"menteslibres.net/gosexy/rest"
+	"net/http/cookiejar"
+	"net/url"
+	"crypto/hmac"
+	"crypto/sha256"
+	"sort"
+	"strings"
+	"encoding/base64"
+	"errors"
+	"encoding/json"
+)
+
+const (
+	SIGN_HEADER = "Content-HMAC"
+)
+
+type Gest struct {
+	secretKey string
+	*rest.Client
+}
+
+func New(secretKey string, header http.Header) (g *Gest) {
+	g = new(Gest)
+	g.secretKey = secretKey
+	g.Header = header
+	g.CookieJar, _ = cookiejar.New(nil)
+	return
+}
+
+func (g *Gest) Get(res interface {}, path string, params url.Values) error {
+	reqSign := g.reqSign("GET", path, params)
+	g.Header.Set(SIGN_HEADER, reqSign)
+	var response rest.Response
+	if err := g.Client.Get(&response, path, params); err != nil {
+		return err
+	}
+	return g.decode(reqSign, response, res)
+}
+
+func (g *Gest) Post(res interface {}, path string, params url.Values) error {
+	reqSign := g.reqSign("POST", path, params)
+	g.Header.Set(SIGN_HEADER, reqSign)
+	var response rest.Response
+	if err := g.Client.Post(&response, path, params); err != nil {
+		return err
+	}
+	return g.decode(reqSign, response, res)
+}
+
+func (g *Gest) Put(res interface {}, path string, params url.Values) error {
+	reqSign := g.reqSign("PUT", path, params)
+	g.Header.Set(SIGN_HEADER, reqSign)
+	var response rest.Response
+	if err := g.Client.Put(&response, path, params); err != nil {
+		return err
+	}
+	return g.decode(reqSign, response, res)
+}
+
+func (g *Gest) Delete(res interface {}, path string, params url.Values) error {
+	reqSign := g.reqSign("DELETE", path, params)
+	g.Header.Set(SIGN_HEADER, reqSign)
+	var response rest.Response
+	if err := g.Client.Delete(&response, path, params); err != nil {
+		return err
+	}
+	return g.decode(reqSign, response, res)
+}
+
+func (g *Gest) reqSign(method, uri string, params url.Values) (string) {
+	mac := hmac.New(sha256.New, []byte(g.secretKey))
+	mac.Write([]byte(method))
+	u, _ := url.Parse(uri)
+	mac.Write([]byte(u.Path))
+
+	if params != nil {
+		var keys []string
+		for key := range params {
+			keys = append(keys, key)
+		}
+		sort.Strings(keys)
+		for _, key := range keys {
+			mac.Write([]byte(key + "=" + strings.Join(params[key], ",")))
+		}
+	}
+	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
+}
+
+func (g *Gest) resSign(reqSign string, body []byte) string {
+	mac := hmac.New(sha256.New, []byte(g.secretKey))
+	mac.Write([]byte(reqSign))
+	mac.Write(body)
+	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
+}
+
+func (g *Gest) decode(reqSign string, response rest.Response, res interface {}) error {
+	actual := response.Header.Get(SIGN_HEADER)
+	expect := g.resSign(reqSign, response.Body)
+	if actual != expect {
+		return errors.New("invalid sign")
+	}
+	return json.Unmarshal(response.Body, res)
+}
